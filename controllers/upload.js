187e847f1fa9c4e38cbbc96ceb72f1ef437ee7a9
yu <mark2020.yu@gmail.com>
2022-06-10 18:58:15 +0800

    w07 modify file check issue

--- controllers/upload.js
+++ controllers/upload.js
@@ -4,8 +4,11 @@ const { ImgurClient } = require("imgur");
 
 const upload = {
 	async uploadImage(req, res, next) {
-		if (!req.files.length) {
-			return next(appError(400, "尚未上傳檔案", next));
+		if (!req.files) {
+			return next(appError(400, "尚未提供任何上傳檔案資料", next));
+		}
+		if (req.files[0]?.size > 2*1024*1024) {
+			return next(appError(400, "圖檔過大，檔案超過2MB.....!", next));
 		}
 		// const dimensions = sizeOf(req.files[0].buffer);
 		// if (dimensions.width !== dimensions.height) {
@@ -14,16 +17,16 @@ const upload = {
 		const client = new ImgurClient({
 			clientId: process.env.IMGUR_CLIENTID,
 			clientSecret: process.env.IMGUR_CLIENT_SECRET,
-			refreshToken: process.env.IMGUR_REFRESH_TOKEN
+			refreshToken: process.env.IMGUR_REFRESH_TOKEN,
 		});
 		const response = await client.upload({
 			image: req.files[0].buffer.toString("base64"),
 			type: "base64",
-			album: process.env.IMGUR_ALBUM_ID
+			album: process.env.IMGUR_ALBUM_ID,
 		});
 		res.status(200).json({
 			status: "success",
-			imgUrl: response.data.link
+			imgUrl: response.data.link,
 		});
 	},
 };
